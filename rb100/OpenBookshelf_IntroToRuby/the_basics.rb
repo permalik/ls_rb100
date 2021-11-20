d184ff73b95c777200c60983f5693a0b8f33b8f0
permalik <permalik@protonmail.com>
2021-11-20 12:56:34 -0600

    Complete float_squared_calculator

--- rb100/OpenBookshelf_IntroToRuby/the_basics.rb
+++ rb100/OpenBookshelf_IntroToRuby/the_basics.rb
@@ -213,3 +213,78 @@ def factorial_calculator
 end
 =end
 
+=begin
+# PARAMETERS #
+# Write a program that calculates the squares of
+# 3 float numbers of your choosing and
+# outputs the result to the screen
+
+## PSEUDO ##
+## CREATE new constant: CONTINUE_LIMIT
+## CREATE new variable: continue_token
+## CREATE new array: user_floats
+
+## PROMPT user: for a floating point number
+##    LOOP: UNTIL continue_token REACHES CONTINUE_LIMIT
+## GET user input: assign to variable and trim /n
+## CHECK input: ensure it is floating point
+## IF invalid entry: re-prompt ELSIF valid entry: push variable onto array
+##    LOOP: CLOSED
+
+## BUILD method: 001 calculate_squares
+## ACCEPT single float as argument: number from user_floats
+## CREATE new variable: squared_value
+## CALCULATE number**2: assign result to squared_value
+## RETURN: squared_value
+
+## BUILD method: 002 convert_user_array
+## ACCEPT single array as argument: user_floats
+## CREATE new array: squared_floats
+##    ITERATE through each number of passed array: user_floats
+## ASSIGN the result from the following calculation: to respective squared_floats index position
+## CALCULATE: method_001(number)
+##    ITERATION: CLOSED
+## RETURN: squared_floats
+##
+## PRINT method_002(user_floats): to user screen
+
+def float_squared_calculator
+  continue_limit = 0
+  continue_token = 3
+  user_floats = []
+  user_float = nil
+
+  def number?(obj)
+    obj = obj.to_s unless obj.is_a? String
+    /\A[+-]?\d+(\.[\d]+)?\z/.match obj
+  end
+
+  loop do
+    $stdout.sync = true
+    puts 'Please input a float:'
+
+    loop do
+      user_float = gets.chomp
+      break if number?(user_float)
+      puts 'That\'s not a number. Please input a float:'
+    end
+
+    user_floats.push(Float(user_float))
+
+    continue_token -= 1
+
+    break if continue_token == continue_limit
+  end
+
+  def calculate_squares(float)
+    float**2
+  end
+
+  def convert_user_array(array)
+    array.collect { |number| calculate_squares(number) }
+  end
+
+  puts convert_user_array(user_floats)
+
+end
+=end

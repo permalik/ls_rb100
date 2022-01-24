ed9fd53b48084c212e47b1b4f3ce6ae2139a0d5c
Malik <permalik@protonmail.com>
2022-01-24 06:24:15 -0600

    Complete name module: part 3

--- name.rb
+++ name.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
-puts 'Input your name'
-user_name = gets.chomp
-puts "Hey there, #{user_name}"
+
+puts 'Input your first name'
+user_first_name = gets.chomp
+
+puts 'Input your last name'
+user_last_name = gets.chomp
+
+user_full_name = "#{user_first_name} #{user_last_name}"
+
+puts "Hey there, #{user_full_name}"
 
 10.times do
-  puts user_name
+  puts user_full_name
 end

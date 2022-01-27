4fc03bcef7a2369ac0bd63d655f36f46ae5f038d
Malik <permalik@protonmail.com>
2022-01-27 15:16:26 -0600

    Complete countdown_for module

---
+++ countdown_for.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# puts 'Input a number greater than 1'
+# number = gets.chomp.to_i
+#
+# for i in 1..number do
+#   puts number  - i
+# end
+
+array = [1, 2, 3, 4, 5]
+
+for i in array.reverse do
+  puts i
+end
+
+puts 'Complete!'

7d464dd899450dfd8d4f40846649e29a87c44e74
Malik <permalik@protonmail.com>
2022-01-30 15:25:09 -0600

    Complete insert_numbers module

---
+++ insert_numbers.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+numbers = []
+
+loop do
+  puts 'Enter any number:'
+  input = gets.chomp.to_i
+  numbers.push(input)
+  break if numbers.size == 5
+end
+puts numbers

dcd258a3a23d59de71bae52795dcbeae49ad2b25
Malik <permalik@protonmail.com>
2022-01-30 06:46:05 -0600

    Complete loopception module

---
+++ loopception.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+loop do
+  puts 'This is the outer loop.'
+
+  loop do
+    puts 'This is the inner loop.'
+    break
+  end
+  break
+end
+
+puts 'This is outside all loops.'

ad31485caf3fc9bc155e6290f48ed829df7ab5ce
Malik <permalik@protonmail.com>
2022-02-06 14:25:14 -0600

    Complete password module

--- password.rb
+++ password.rb
@@ -1,16 +1,20 @@
 # frozen_string_literal: true
 
+TARGET_USERNAME = 'secret username'
 TARGET_PASSWORD = 'secret password'
 welcome_message = 'Successful, welcoming secret message!'
 
 loop do
-  puts 'Input the secret password: '
-  user_input = gets.chomp.downcase
+  puts 'Input the secret username: '
+  input_username = gets.chomp.downcase
 
-  if user_input == TARGET_PASSWORD
+  puts 'Input the secret password: '
+  input_password = gets.chomp.downcase
+
+  if input_username == TARGET_USERNAME && input_password == TARGET_PASSWORD
     puts welcome_message
     break
   end
 
-  puts 'Invalid password. Try again...'
+  puts 'Invalid input. Try again...'
 end

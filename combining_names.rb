41e04f4f853a8c679c69cc6b51b1b5ba9d9fe705
Malik <permalik@protonmail.com>
2022-02-11 06:35:18 -0600

    Complete combining_names module

---
+++ combining_names.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+first_name = 'John'
+last_name = 'Doe'
+
+first_name_two = 'Jane'
+last_name_two = 'Smith'
+
+full_name = "#{first_name} #{last_name}"
+
+full_name_two = first_name_two + ' ' + last_name_two
+
+puts full_name
+puts full_name_two

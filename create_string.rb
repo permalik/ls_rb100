cea9edd2b20d97e71c3a7741d29315f288209a13
Malik <permalik@protonmail.com>
2022-02-11 05:58:44 -0600

    Complete create_string module

---
+++ create_string.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+empty_string_class = String.new
+
+empty_string = ''
+
+puts 'Successful empty string!' if empty_string_class
+puts 'Successful empty string!' if empty_string
